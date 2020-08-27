73b52980d0c165826043f41b0e311b2050a18eda
Marcella Luz Ash <marcella@ash-photos.com>
2020-08-26 18:09:03 -0700

    changed eaten to devoured

--- public/app.js
+++ public/app.js
@@ -4,7 +4,7 @@ document.getElementById('newBurger').addEventListener('click', event => {
   axios.post('/api/burgers', {
     burger_name: document.getElementById('burger').
     value,
-    notEaten: false
+    notDevoured: false
   }).
     .then(({data}) => {
       let burgerElem = document.createElement('li')
@@ -17,16 +17,16 @@ document.getElementById('newBurger').addEventListener('click', event => {
           data-burger_name="${document.getElementById('burger').value}">Devour it!</button>
         </div>
       `
-      document.getElementById('notEaten').append(burgerElem)
+      document.getElementById('notDevoured').append(burgerElem)
       document.getElementById('burger').value = ''
     })
     .catch(err => console.log(err))
 })
 
 document.addEventListener('click', event => {
-  if (event.target.classList.contains('eaten')) {
+  if (event.target.classList.contains('devoured')) {
     axios.put(`/api/burgers/${event.target.parentNode.parentNode.id}`, {
-      eaten: true
+      devoured: true
     })
     .then(() => {
       let burgerElem = document.createElement('li')
@@ -37,7 +37,7 @@ document.addEventListener('click', event => {
             <h4 class="mb-1">${event.target.dataset.burger_name}</h4>
         </div>
       `
-      document.getElementById('eaten').append(burgerElem)
+      document.getElementById('devoured').append(burgerElem)
       event.target.parentNode.parentNode.remove()
     })
     .catch(err => console.log(err))
