f908858894a315744c1374a2353f9f1670b1368a
Marcella Luz Ash <marcella@ash-photos.com>
2020-08-28 15:13:34 -0700

    formatting

--- server.js
+++ server.js
@@ -7,8 +7,7 @@ app.use(express.static(join(__dirname, 'public')))
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 
-app.engine('.hbs', require('express-handlebars')
-  ({ extname: '.hbs' }))
+app.engine('.hbs', require('express-handlebars')({ extname: '.hbs' }))
 app.set('view engine;', '.hbs')
 
 app.use(require('./controllers'))
